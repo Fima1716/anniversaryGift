2d284f09b86d49a15cba81000f201f92507a6a09
Fima1716 <efimt74@gmail.com>
2023-08-25 14:08:05 +0300

    v6

--- js/script.js
+++ js/script.js
@@ -8,26 +8,49 @@ function updateCountdown() {
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-    const daysElement = document.getElementById("days");
-    const hoursElement = document.getElementById("hours");
-    const minutesElement = document.getElementById("minutes");
-    const secondsElement = document.getElementById("seconds");
-
-    updateElementWithAnimation(daysElement, days);
-    updateElementWithAnimation(hoursElement, hours);
-    updateElementWithAnimation(minutesElement, minutes);
-    updateElementWithAnimation(secondsElement, seconds);
+    updateElementWithAnimation("days", days);
+    updateElementWithAnimation("hours", hours);
+    updateElementWithAnimation("minutes", minutes);
+    updateElementWithAnimation("seconds", seconds);
 }
 
-function updateElementWithAnimation(element, newValue) {
-    if (element.textContent !== newValue) {
-        element.classList.add("blink");
+function updateElementWithAnimation(id, newValue) {
+    const element = document.getElementById(id);
+    const currentValue = element.textContent;
+
+    if (currentValue !== newValue) {
+        element.textContent = newValue.toString().padStart(2, "0");
+        element.classList.add();
+
         setTimeout(() => {
-            element.textContent = newValue.toString().padStart(2, "0");
-            element.classList.remove("blink");
+            element.classList.remove();
         }, 300);
     }
 }
 
 updateCountdown();
 setInterval(updateCountdown, 1000);
+
+
+// Найти новую кнопку-сердечко и модальное окно по их id
+const heartButton = document.querySelector(".heart-button");
+const modal = document.getElementById("modal");
+const closeModalButton = document.getElementById("close-modal");
+
+heartButton.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+// Закрыть модальное окно при клике на кнопку "Закрыть"
+closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Закрыть модальное окно при клике за его пределами
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+
